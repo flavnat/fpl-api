5d9fd81d8a92f05e67166627ee41aec993328b0e
flavnat <natnael01ayele@gmail.com>
2025-12-22 20:49:14 +0300

    feat: add sync state schema"

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -3,6 +3,7 @@ import * as elementTypes from '../modules/element-types/element-types.schema.js'
 import * as elements from '../modules/elements/elements.schema.js'
 import * as events from '../modules/events/events.schema.js'
 import * as fixtures from '../modules/fixtures/fixtures.schema.js'
+import * as syncState from '../modules/sync-state/sync-state.schema.js'
 import * as teams from '../modules/teams/teams.schema.js'
 
 export * from '../modules/auth/auth.schema.js'
@@ -10,6 +11,7 @@ export * from '../modules/element-types/element-types.schema.js'
 export * from '../modules/elements/elements.schema.js'
 export * from '../modules/events/events.schema.js'
 export * from '../modules/fixtures/fixtures.schema.js'
+export * from '../modules/sync-state/sync-state.schema.js'
 export * from '../modules/teams/teams.schema.js'
 
 export const schema = {
@@ -19,4 +21,5 @@ export const schema = {
   ...elements,
   ...events,
   ...fixtures,
+  ...syncState,
 }
