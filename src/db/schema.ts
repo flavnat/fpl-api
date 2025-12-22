b91a6cd34619112778d462d84c51723be410737b
flavnat <natnael01ayele@gmail.com>
2025-12-22 16:34:54 +0300

    feat: add teams schema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,7 +1,10 @@
 import * as auth from '../modules/auth/auth.schema.js'
+import * as teams from '../modules/teams/teams.schema.js'
 
 export * from '../modules/auth/auth.schema.js'
+export * from '../modules/teams/teams.schema.js'
 
 export const schema = {
   ...auth,
+  ...teams,
 }
