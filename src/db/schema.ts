67bdc7459f86ae15b0473717896487fb0d9016c7
flavnat <natnael01ayele@gmail.com>
2025-12-22 19:48:47 +0300

    feat: add elements schema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,13 +1,16 @@
 import * as auth from '../modules/auth/auth.schema.js'
 import * as elementTypes from '../modules/element-types/element-types.schema.js'
+import * as elements from '../modules/elements/elements.schema.js'
 import * as teams from '../modules/teams/teams.schema.js'
 
 export * from '../modules/auth/auth.schema.js'
 export * from '../modules/element-types/element-types.schema.js'
+export * from '../modules/elements/elements.schema.js'
 export * from '../modules/teams/teams.schema.js'
 
 export const schema = {
   ...auth,
   ...teams,
   ...elementTypes,
+  ...elements,
 }
