990bf4e41ba6c34782435a2d83de3d746a0c515d
flavnat <natnael01ayele@gmail.com>
2025-12-22 20:04:52 +0300

    feat: fixtures schema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -2,12 +2,14 @@ import * as auth from '../modules/auth/auth.schema.js'
 import * as elementTypes from '../modules/element-types/element-types.schema.js'
 import * as elements from '../modules/elements/elements.schema.js'
 import * as events from '../modules/events/events.schema.js'
+import * as fixtures from '../modules/fixtures/fixtures.schema.js'
 import * as teams from '../modules/teams/teams.schema.js'
 
 export * from '../modules/auth/auth.schema.js'
 export * from '../modules/element-types/element-types.schema.js'
 export * from '../modules/elements/elements.schema.js'
 export * from '../modules/events/events.schema.js'
+export * from '../modules/fixtures/fixtures.schema.js'
 export * from '../modules/teams/teams.schema.js'
 
 export const schema = {
@@ -16,4 +18,5 @@ export const schema = {
   ...elementTypes,
   ...elements,
   ...events,
+  ...fixtures,
 }
