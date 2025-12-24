56d3720694635c9adc2e961fa1f58a1f505171fd
flavnat <natnael01ayele@gmail.com>
2025-12-24 14:45:13 +0300

    feat: name utils

--- src/utils/elements-utils.ts
+++ src/utils/elements-utils.ts
@@ -6,6 +6,6 @@ export function getPlayerPhotoUrl(photo?: string): string {
   if (!photo)
     return ''
 
-  const nameWithoutExt = photo.replace(/\.[^/.]+$/, '') // removes any extension
+  const nameWithoutExt = photo.replace(/\.[^/.]+$/, '')
   return `https://resources.premierleague.com/premierleague25/photos/players/110x140/${nameWithoutExt}.png`
 }
